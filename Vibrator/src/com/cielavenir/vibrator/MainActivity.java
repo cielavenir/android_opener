72499d5c36050473d906e7ce4ae1d93e3c6b8c14
cielavenir <cielartisan@gmail.com>
2014-01-18 21:56:59 +0900

    Vibrator correctly stops if backed, continues to work in sleep

--- Vibrator/src/com/cielavenir/vibrator/MainActivity.java
+++ Vibrator/src/com/cielavenir/vibrator/MainActivity.java
@@ -1,26 +1,52 @@
 package com.cielavenir.vibrator;
 
 import android.os.Bundle;
+import android.os.Process;
 import android.os.Vibrator;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
+import android.view.KeyEvent;
 
 public class MainActivity extends Activity {
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-        long[] pattern = {500, 1000};
-        vibrator.vibrate(pattern, 0);
-	}
+		final Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+		final long[] pattern = {500, 1000};
+		vibrator.vibrate(pattern, 0);
+		BroadcastReceiver vibrateReceiver = new BroadcastReceiver(){
+			@Override
+			public void onReceive(Context context, Intent intent){
+				if(intent.getAction().equals(Intent.ACTION_SCREEN_OFF)){
+					vibrator.vibrate(pattern, 0);
+				}
+			}
+		};
 
+		IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
+		registerReceiver(vibrateReceiver , filter);
+	}
+/*
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+*/
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event){
+		//Log.d("Vibrator",String.valueOf(keyCode));
+		if (keyCode == KeyEvent.KEYCODE_BACK){
+			//this.finish();
+			Process.killProcess(Process.myPid()); //process must be stopped, not suspended
+			return true;
+		}
+		return false;
+	}
 }
